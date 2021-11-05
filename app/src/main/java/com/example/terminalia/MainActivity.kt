35ab394905f68ae0c23d3e0997cb9c9109f1dd85
SadLoneWolf <djordjeandjic99@gmail.com>
2021-11-05 14:30:41 +0300

    Connected to Firebase

--- app/src/main/java/com/example/terminalia/MainActivity.kt
+++ app/src/main/java/com/example/terminalia/MainActivity.kt
@@ -4,16 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.button2)
-
+        val db = Firebase.database
+        val ref = db.reference
+        ref.setValue("Connection sucessfull!")
         button.setOnClickListener{
             Intent(this,Login::class.java).also {
                 startActivity(it)
+
             }
         }
     }
