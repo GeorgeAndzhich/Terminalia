150eea4bf358a6768c6b379841d15fc7a1583e37
SadLoneWolf <djordjeandjic99@gmail.com>
2022-02-03 00:58:40 +0300

    Redo Sign UP

--- app/src/main/java/com/example/terminalia/SignUp.kt
+++ app/src/main/java/com/example/terminalia/SignUp.kt
@@ -2,12 +2,7 @@ package com.example.terminalia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Looper
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -16,25 +11,26 @@ class SignUp : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_sign_up2)
         getSupportActionBar()?.hide()
         var mail= findViewById<EditText>(R.id.editTextTextEmailAddress).text.toString()
         var password = findViewById<EditText>(R.id.editTextTextPassword).text.toString()
         val signUpbutton = findViewById<Button>(R.id.button3)
         signUpbutton.setOnClickListener{
-            GlobalScope.launch(Dispatchers.Main) {
-                SignUp(mail,password)
+           GlobalScope.launch(Dispatchers.IO) {
+               SignUp(mail,password)
+           }
+            }
+    }
+
+    suspend fun  SignUp(mail:String,password:String){
+        auth.createUserWithEmailAndPassword(mail,password).addOnCompleteListener{
+            if (it.Sucessfull){
+                Toast.makeText(baseContext,"User registered sucessfully!",Toast.LENGTH_LONG).show()
+                finish()
+            } else {
+                Toast.makeText(baseContext,"User  not registered due to ${it.exception}",Toast.LENGTH_LONG).show()
             }
         }
-    }
-    suspend fun  SignUp(mail:String,password:String){
-            auth.createUserWithEmailAndPassword(mail,password).addOnCompleteListener{
-                if (it.isSuccessful){
-                    Toast.makeText(baseContext,"User registered sucessfully!",Toast.LENGTH_LONG).show()
-                    finish()
-            } else {
-                    Toast.makeText(baseContext,"User  not registered due to ${it.exception}",Toast.LENGTH_LONG).show()
-                }
-            }
     }
 }
